80a9cd568e880a97b49747cbd6f0326a312258e1
Jackson Allen <79735432+ModBoyex@users.noreply.github.com>
2026-01-15 20:44:57 -0600

    Initial commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2026_Template";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 8;
+    public static final String GIT_SHA = "ceae389f9e78683566ccb645485d109bb4f29188";
+    public static final String GIT_DATE = "2026-01-15 20:39:45 EST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2026-01-15 20:53:36 EST";
+    public static final long BUILD_UNIX_TIME = 1768528416533L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
