c14fdcb3c401e9e1469776c5a1e80d54b9b67ab2
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2026-01-17 12:03:05 -0600

    Example layout for 2026

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuild_2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 1;
-    public static final String GIT_SHA = "80a9cd568e880a97b49747cbd6f0326a312258e1";
-    public static final String GIT_DATE = "2026-01-15 21:44:57 EST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-01-16 14:53:28 EST";
-    public static final long BUILD_UNIX_TIME = 1768593208453L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 2;
+    public static final String GIT_SHA = "3da1e7bd76bd8ceac17a2f57f143ddfee77c7ebf";
+    public static final String GIT_DATE = "2026-01-16 14:53:48 EST";
+    public static final String GIT_BRANCH = "Initial-Layout-and-Utils";
+    public static final String BUILD_DATE = "2026-01-17 12:51:57 EST";
+    public static final long BUILD_UNIX_TIME = 1768672317566L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
---
+++ src/main/java/frc/robot/subsystems/example/exampleConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.example;
+
+public class exampleConstants {
+    /* Store all constant values required for a subsystem besides motor ID values and similar items in a subsystems designated constants file
+     * if you aren't sure put it here just to be safe
+     */
+}
