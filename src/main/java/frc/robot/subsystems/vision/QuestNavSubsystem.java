c2726202bb0670907a6e6276ccb969edbfcd5423
Anshu <acharya.anshu@gmail.com>
2026-01-21 20:31:39 -0600

    Add initial pose setup to QuestNav subsystem and update RobotContainer

--- src/main/java/frc/robot/subsystems/vision/QuestNavSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/QuestNavSubsystem.java
@@ -2,11 +2,14 @@ package frc.robot.subsystems.vision;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drive.Drive;
 import gg.questnav.questnav.PoseFrame;
@@ -17,6 +20,7 @@ public class QuestNavSubsystem extends SubsystemBase {
     private final QuestNav questNav;
     private final Drive drivetrain;
     private Pose3d robotPose;
+    private Pose2d initialPose;
     private Pose3d questPose;
     private static final Transform3d ROBOT_TO_QUEST = new Transform3d(0.0, 0.0, 0.0, new Rotation3d());
     private static final Matrix<N3, N1> QUESTNAV_STD_DEVS = VecBuilder.fill(0.02, 0.02, 0.035);
@@ -51,5 +55,10 @@ public class QuestNavSubsystem extends SubsystemBase {
     public void resetPose(Pose3d robotPose) {
         Pose3d questPose = robotPose.transformBy(ROBOT_TO_QUEST);
         questNav.setPose(questPose);
+    }
+    
+
+    public void setStartPose (Pose2d initialPose) {
+        this.initialPose = initialPose;
     }
 }
