32e4e8d30e52ff8c9d0cf59025e3eef4af28227a
Allen Piscitello <apiscitello@synota.io>
2026-01-31 12:04:07 -0600

    more clear comments

--- src/main/java/frc/robot/util/OILayer/OI.java
+++ src/main/java/frc/robot/util/OILayer/OI.java
@@ -29,61 +29,69 @@ public interface OI {
     /* Puts the shooter into a mode where it is able to shoot (e.g. spins up a flywheel that was currently idle) 
      * Rumbles joystick or turns on lights on the robot when it gets up to speed to be able to shoot
      * When button is release, return to an idle speed
-    */
+     * Subsystem: Shooter - set flywheel motor(s) to "shooting speed" based on distance, set to idle speed when released
+     */
     default Trigger spinUpShooter() {
         return noButton;
     }
 
 
-    /* Hold this button to fire shooter. Make sure the shooter is spun up before allowing this  */
+    /* Hold this button to fire shooter. Make sure the shooter is spun up before allowing this 
+     * 
+     * Subsystem: Shooter - turn on the feeding motor(s) to enable shooting to occur
+     */
     default Trigger fireShooter() {
         return noButton;
     }
 
-    /* Run the rollers on the intake while held */
+    /* Run the rollers on the intake while held 
+     * 
+     * Subsystem: Intake - spin the rollers forward when held, stop spinning when released
+    */
     default DoubleSupplier intake() {
         return noAxis;
     }
 
     /* Run the rollers in reverse while held */
+    /* Subsystem: Intake - spin the rollers backwards when held, stop spinning when released */
     default DoubleSupplier outtake() {
         return noAxis;
     }
 
-    /* Push the button to make the intake extend from the robot*/
-    default Trigger extendIntake() {
-        return noButton;
-    }
-
-    /* Push the button to make the intake retract
-     * We need to make sure that nothing breaks when retracting if balls are in the hopper, 
-     * if this is also tied to the hopper.
-     * Have a safety mechanism if it is not retracting that it goes back out.
-     */
-    default Trigger retractIntake() {
+    /* When pressed, the intake will extend, and when pressed again, it will retract
+    /* Be careful when retracting in case the hopper is full of balls, that it should 
+    /* be able to avoid damaging the robot if things are in the way.
+    /* Subsystem: Intake
+    */
+    default Trigger toggleIntakeState() {
         return noButton;
     }
 
 
-    /* While this button is held, the robot aims for the hub and prevents the driver from updating the rotation */
+    /* While this button is held, the robot aims for the hub and prevents the driver from updating the rotation 
+     * Subsystem: Drive - whenever this is held down, set rotation based on position
+    */
     default Trigger lockOnTarget() {
         return noButton;
     }
 
     /* Press this button to get off of the rungs in auton mode (raise the elevator) */
+    /* Subsystem: Climber */
     default Trigger declimb() {
         return noButton;
     }
 
-    
+    /* Subsystmem: Climber */
     default Trigger climb_l1() {
         return noButton;
     }
 
+    /* Subsystem: Climber */
     default Trigger climb_l2() {
         return noButton;
     }
 
+    /* Subsystem: Climber */
     default Trigger climb_l3() {
         return noButton;
     }
