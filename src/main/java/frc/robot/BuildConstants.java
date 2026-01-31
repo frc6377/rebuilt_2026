5fe17c327c0f99cec55b03947f9ba2f861242e9c
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2026-01-31 11:05:27 -0600

    Io implementation (#6)
    * Example layout for 2026
    
    * Add OI layer we began using in rodeo
    
    * Create IO files for currently complete subsystems
    
    * Do what I was supposed to
    
    ---------
    
    Co-authored-by: KianChilds <94756266+KianChilds@users.noreply.github.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -8,10 +8,10 @@ public final class BuildConstants {
     public static final int GIT_REVISION = 2;
     public static final String GIT_SHA = "3da1e7bd76bd8ceac17a2f57f143ddfee77c7ebf";
     public static final String GIT_DATE = "2026-01-16 14:53:48 EST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2026-01-17 13:05:48 EST";
-    public static final long BUILD_UNIX_TIME = 1768673148224L;
-    public static final int DIRTY = 1;
+    public static final String GIT_BRANCH = "Initial-Layout-and-Utils";
+    public static final String BUILD_DATE = "2026-01-17 12:51:57 EST";
+    public static final long BUILD_UNIX_TIME = 1768672317566L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
