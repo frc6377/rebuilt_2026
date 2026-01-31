958f0c484f7af1311f69c47025fbf61edaee7eb7
Allen Piscitello <apiscitello@synota.io>
2026-01-31 11:47:19 -0600

    documenting commands and adding missing commands

--- src/main/java/frc/robot/util/OILayer/OI.java
+++ src/main/java/frc/robot/util/OILayer/OI.java
@@ -26,31 +26,65 @@ public interface OI {
         return noButton;
     }
 
-    default DoubleSupplier spinUpShooter() {
-        return noAxis;
+    /* Puts the shooter into a mode where it is able to shoot (e.g. spins up a flywheel that was currently idle) 
+     * Rumbles joystick or turns on lights on the robot when it gets up to speed to be able to shoot
+     * When button is release, return to an idle speed
+    */
+    default Trigger spinUpShooter() {
+        return noButton;
     }
 
+
+    /* Hold this button to fire shooter. Make sure the shooter is spun up before allowing this  */
     default Trigger fireShooter() {
         return noButton;
     }
 
+    /* Run the rollers on the intake while held */
     default DoubleSupplier intake() {
         return noAxis;
     }
 
+    /* Run the rollers in reverse while held */
     default DoubleSupplier outtake() {
         return noAxis;
     }
 
+    /* Push the button to make the intake extend from the robot*/
     default Trigger extendIntake() {
         return noButton;
     }
 
+    /* Push the button to make the intake retract
+     * We need to make sure that nothing breaks when retracting if balls are in the hopper, 
+     * if this is also tied to the hopper.
+     * Have a safety mechanism if it is not retracting that it goes back out.
+     */
+    default Trigger retractIntake() {
+        return noButton;
+    }
+
+
+    /* While this button is held, the robot aims for the hub and prevents the driver from updating the rotation */
     default Trigger lockOnTarget() {
         return noButton;
     }
 
+    /* Press this button to get off of the rungs in auton mode (raise the elevator) */
     default Trigger declimb() {
+        return noButton;
+    }
+
+    
+    default Trigger climb_l1() {
+        return noButton;
+    }
+
+    default Trigger climb_l2() {
+        return noButton;
+    }
+
+    default Trigger climb_l3() {
         return noButton;
     }
 }
