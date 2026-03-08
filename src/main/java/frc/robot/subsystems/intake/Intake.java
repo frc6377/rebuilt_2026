ab5987093e6e1c1107e20412c1b4d309f77ae964
Jameson Piscitello <piscitello284@gmail.com>
2026-03-08 14:12:48 -0500

    Update at 'Sun Mar 08 14:12:48 CDT 2026'

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -165,14 +165,14 @@ public class Intake extends SubsystemBase {
     }
 
     public Command voltageSiftFuel() {
-        return Commands.runOnce(roller::start).andThen(
-        Commands.repeatingSequence(
-                run(() -> extender.currentRunShoot(-1))
-                        .until(() -> extender.getCurrent().gte(Amps.of(8)))
-                        .withTimeout(2),
-                run(() -> extender.currentRunShoot(1))
-                        .until(() -> extender.getCurrent().gte(Amps.of(8)))
-                        .withTimeout(2)));
+        return Commands.runOnce(roller::start)
+                .andThen(Commands.repeatingSequence(
+                        run(() -> extender.currentRunShoot(-1))
+                                .until(() -> extender.getCurrent().gte(Amps.of(8)))
+                                .withTimeout(2),
+                        run(() -> extender.currentRunShoot(1))
+                                .until(() -> extender.getCurrent().gte(Amps.of(8)))
+                                .withTimeout(2)));
     }
 
     public Command currentRunDescendNoCheck() {
