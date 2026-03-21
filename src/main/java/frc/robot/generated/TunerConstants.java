268a3833d5b7b5fbea9b0cb63419232d12c6ea76
Pranav R <pranavr@howdybots.org>
2026-03-21 11:41:46 -0500

    spare pod values

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -150,6 +150,13 @@ public class TunerConstants {
                     .withDriveInertia(kDriveInertia)
                     .withSteerFrictionVoltage(kSteerFrictionVoltage)
                     .withDriveFrictionVoltage(kDriveFrictionVoltage);
+/*
+ * Spare Modules
+ * Pod 5: 
+ *      Drive: 31
+ *      Steer: 30
+ *      Encoder: 30
+ */
 
     // Front Left
     private static final int kFrontLeftDriveMotorId = 1;
