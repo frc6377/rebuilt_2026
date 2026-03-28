7d7491eceeb22fb319b817e4dc1bbe6d6607beb2
Pranav R <pranavr@howdybots.org>
2026-03-28 18:34:24 -0500

    Update at 'Sat Mar 28 18:34:24 CDT 2026'

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,7 +41,7 @@ public class Robot extends LoggedRobot {
         switch (Constants.currentMode) {
             case REAL:
                 // Running on a real robot, log to a USB stick ("/U/logs")
-                Logger.addDataReceiver(new WPILOGWriter());
+                Logger.addDataReceiver(new WPILOGWriter("U/sda1"));
                 Logger.addDataReceiver(new NT4Publisher());
                 break;
 
