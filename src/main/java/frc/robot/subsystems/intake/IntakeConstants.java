3416e572b92c7bc79f28ce48c0a31d9b33f74540
Pranav R <pranavr@howdybots.org>
2026-03-28 10:31:07 -0500

    Update at 'Sat Mar 28 10:31:07 CDT 2026'

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -58,8 +58,7 @@ public class IntakeConstants {
         public static final Angle kExtenderSiftAngleTwo = Degrees.of(60.0);
         public static final Angle kExtenderCustomAngleOne = Degrees.of(45.0);
         public static final Angle kExtenderCustomAngleTwo = Degrees.of(60.0);
-        public static final Angle kExtenderZeroAngle = Degrees.of(-284/5);
-        
+        public static final Angle kExtenderZeroAngle = Degrees.of(-284);
 
         // Sift Constants
         public static final Current kSiftCurrentLimit = Amps.of(15);
