a3f5964667f656872a5468e30eb9a3b762e14e1a
smijar <smijar@gmail.com>
2026-04-04 11:03:15 -0500

    Update build constants for versioning and synchronization with latest commit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuilt_2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 366;
-    public static final String GIT_SHA = "662a42384c68226e1aee848791558d074666cea8";
-    public static final String GIT_DATE = "2026-03-24 21:01:41 EDT";
+    public static final int GIT_REVISION = 367;
+    public static final String GIT_SHA = "50a24ff559c8e4041a543386957b117b0b7f4229";
+    public static final String GIT_DATE = "2026-03-24 22:32:00 EDT";
     public static final String GIT_BRANCH = "ClimbL3";
-    public static final String BUILD_DATE = "2026-03-24 21:08:51 EDT";
-    public static final long BUILD_UNIX_TIME = 1774400931578L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2026-03-31 19:49:07 EDT";
+    public static final long BUILD_UNIX_TIME = 1775000947458L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
