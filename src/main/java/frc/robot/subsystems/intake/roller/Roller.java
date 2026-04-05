6c5d337acf2ef6fcae9d082be57479f9c8f979c3
Pranav6000 <coolpranav.r@gmail.com>
2026-04-05 17:47:48 -0400

    removed bad sysid

--- src/main/java/frc/robot/subsystems/intake/roller/Roller.java
+++ src/main/java/frc/robot/subsystems/intake/roller/Roller.java
@@ -2,31 +2,23 @@ package frc.robot.subsystems.intake.roller;
 
 import static edu.wpi.first.units.Units.Volts;
 
-import com.ctre.phoenix6.SignalLogger;
+
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
 import org.littletonrobotics.junction.Logger;
 
 public class Roller extends SubsystemBase {
     private final RollerIO io;
     private final RollerIOInputsAutoLogged inputs = new RollerIOInputsAutoLogged();
-    private final SysIdRoutine sysIdRoutine;
+
 
     public Roller(RollerIO io) {
         this.io = io;
         setDefaultCommand(run(io::idle).withName("RollerIdle"));
-        sysIdRoutine = new SysIdRoutine(
-                new SysIdRoutine.Config(
-                        null, // Use default ramp rate (1 V/s)
-                        Volts.of(4), // Reduce dynamic step voltage to 4 to prevent brownout
-                        null, // Use default timeout (10 s)
-                        // Log state with Phoenix SignalLogger class
-                        (state) -> SignalLogger.writeString("state", state.toString())),
-                new SysIdRoutine.Mechanism(io::setRollerVoltage, null, this));
-    }
+}
 
     @Override
     public void periodic() {
@@ -38,13 +30,6 @@ public class Roller extends SubsystemBase {
                 getCurrentCommand() != null ? getCurrentCommand().getName() : "None");
     }
 
-    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
-        return sysIdRoutine.quasistatic(direction);
-    }
-
-    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
-        return sysIdRoutine.dynamic(direction);
-    }
 
     public Command runCommand() {
         return Commands.runEnd(io::start, io::stop, this).withName("RollerRun");
