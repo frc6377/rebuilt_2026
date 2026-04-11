81222ca3eb2861240e6d1cb959291a60344b679b
Anshu <acharya.anshu@gmail.com>
2026-04-11 10:19:15 -0500

    add temp logging for tunable talon fx

--- src/main/java/frc/robot/util/TunableTalonFX.java
+++ src/main/java/frc/robot/util/TunableTalonFX.java
@@ -18,6 +18,10 @@ import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.Units;
+
+import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 /**
@@ -39,6 +43,8 @@ import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
  */
 public class TunableTalonFX extends TalonFX {
     private final String tunableName;
+
+    private final int deviceId;
 
     private final boolean TUNING_ENABLED = false;
 
@@ -71,6 +77,7 @@ public class TunableTalonFX extends TalonFX {
     public TunableTalonFX(int deviceId, String canbus, String tunableName, Slot0Configs initialGains) {
         super(deviceId, new CANBus(canbus));
         this.tunableName = tunableName;
+        this.deviceId = deviceId;
 
         // Create tunable values from Slot0Configs
         this.tunableKP = new LoggedNetworkNumber(tunableName + "/kP", initialGains.kP);
@@ -135,6 +142,10 @@ public class TunableTalonFX extends TalonFX {
             // Apply new gains
             applyTunableGains();
         }
+
+        Logger.recordOutput(
+                "Temp/" + tunableName + "/" + deviceId,
+                getDeviceTemp().getValue().in(Units.Fahrenheit));
 
         return changed;
     }
