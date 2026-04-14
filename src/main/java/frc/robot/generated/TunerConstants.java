708f57e6424d463f775832dc4965a05865a29452
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2026-04-13 21:54:21 -0500

    tests

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -105,7 +105,7 @@ public class TunerConstants {
 
     private static final double kDriveGearRatio = 5.2734375;
     private static final double kSteerGearRatio = 26.09090909090909;
-    private static final Distance kWheelRadius = Inches.of(1.9);
+    private static final Distance kWheelRadius = Inches.of(1.9722);
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
