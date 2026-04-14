3c7fbd6321a99ada7f5a7f9756fc372ea15f214a
Jameson Piscitello <piscitello284@gmail.com>
2026-04-13 19:10:26 -0500

    Refactor rumble scheduling logic for hub state changes (#61)

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -154,14 +154,10 @@ public class Superstructure extends SubsystemBase {
                 "Shooting/DistanceToHub", round(vision.getHubDistanceMeasure().in(Meters) * 100.0) / 100.0);
         if (FieldConstants.getTimeUntilHubStateChange() > 0 && FieldConstants.getTimeUntilHubStateChange() < 1) {
             CommandScheduler.getInstance().schedule(oi.setRumble(0, 0));
-        } else if (FieldConstants.getTimeUntilHubStateChange() > 4.75
-                && FieldConstants.getTimeUntilHubStateChange() < 5) {
-            CommandScheduler.getInstance().schedule(oi.setRumble(0.7, 0.7));
-        } else if (FieldConstants.getTimeUntilHubStateChange() > 4.25
-                && FieldConstants.getTimeUntilHubStateChange() < 4.5) {
-            CommandScheduler.getInstance().schedule(oi.setRumble(0.75, 0.75));
+        } else if (FieldConstants.getTimeUntilHubStateChange() > 4 && FieldConstants.getTimeUntilHubStateChange() < 5) {
+            CommandScheduler.getInstance().schedule(oi.setRumble(1, 1));
         } else {
-            oi.setRumble(0, 0);
+            CommandScheduler.getInstance().schedule(oi.setRumble(0, 0));
         }
 
         if (gamePieceTrajectorySimulation == null) {
