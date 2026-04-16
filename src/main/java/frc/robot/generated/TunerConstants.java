ce83f93974d9a975a6dad4088236d84e5588b336
Pranav R <pranavr@howdybots.org>
2026-04-16 17:15:55 -0500

    Update at 'Thu Apr 16 17:15:54 CDT 2026'

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -173,13 +173,22 @@ public class TunerConstants {
     *   Encoder: 3
     *  Zero: 0.02345
 
-    */
-
-    // Front Left
+      POD 1
     private static final int kFrontLeftDriveMotorId = 1;
     private static final int kFrontLeftSteerMotorId = 2;
     private static final int kFrontLeftEncoderId = 1;
     private static final Angle kFrontLeftEncoderOffset = Rotations.of(-0.406005859375);
+    private static final boolean kFrontLeftSteerMotorInverted = false;
+    private static final boolean kFrontLeftEncoderInverted = false;
+
+
+    */
+
+    // Front Left
+    private static final int kFrontLeftDriveMotorId = 31;
+    private static final int kFrontLeftSteerMotorId = 30;
+    private static final int kFrontLeftEncoderId = 30;
+    private static final Angle kFrontLeftEncoderOffset = Rotations.of(0.28515);
     private static final boolean kFrontLeftSteerMotorInverted = false;
     private static final boolean kFrontLeftEncoderInverted = false;
 
