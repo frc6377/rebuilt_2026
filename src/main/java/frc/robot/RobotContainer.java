f6f05ab3cba8e916a9d23536592c47681857bd84
Pranav R <pranavr@howdybots.org>
2026-06-24 19:46:09 -0500

    Revert "Update at 'Sat Jun 20 10:28:33 CDT 2026'"
    This reverts commit 4fb30bf0260e625857b4a44a94ede056abaebaf6.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -421,17 +421,17 @@ public class RobotContainer {
                         drive, OIController.driveTranslationX(), OIController.driveTranslationY())));
         OIController.hubShootSpeed()
                 .onTrue(superstructure
-                        .setFlywheelVelocityManual(RPM.of(1300))
+                        .setFlywheelVelocityManual(RPM.of(2600))
                         .alongWith(superstructure.setManualShootingEnabledCommand(true))
                         .andThen(superstructure.runFlywheelVelocityManual()));
         OIController.towerShootSpeed()
                 .onTrue(superstructure
-                        .setFlywheelVelocityManual(RPM.of(1600))
+                        .setFlywheelVelocityManual(RPM.of(3200))
                         .alongWith(superstructure.setManualShootingEnabledCommand(true))
                         .andThen(superstructure.runFlywheelVelocityManual()));
         OIController.cornerShootSpeed()
                 .onTrue(superstructure
-                        .setFlywheelVelocityManual(RPM.of(2400))
+                        .setFlywheelVelocityManual(RPM.of(3800))
                         .alongWith(superstructure.setManualShootingEnabledCommand(true))
                         .andThen(superstructure.runFlywheelVelocityManual()));
 
